425e3d08db7be5ce3094e6407aa6bd5a75e49751
wikimodels <55264797+wikimodels@users.noreply.github.com>
2024-04-02 07:11:20 +0300

    added gauge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'ExchangeScreener';
+
+  gaugeType = 'semi';
+  gaugeValue = 28.3;
+  gaugeLabel = 'Speed';
+  gaugeAppendText = 'km/hr';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppMaterialModule } from './material.module';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NgxGaugeModule } from 'ngx-gauge';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, NavBarComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AppMaterialModule,
+    AppRoutingModule,
+    NgxGaugeModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
