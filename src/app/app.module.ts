7b0377af9ae1e93069e7cee2ee959f592a9fedca
wikimodels <55264797+wikimodels@users.noreply.github.com>
2024-05-02 14:18:21 +0300

    GOT FREAKING CANDLECHART!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { NgxEchartsModule } from 'ngx-echarts';
+
+import { CanvasRenderer } from 'echarts/renderers';
+import Marcaron from './marcaron';
+
+import * as echarts from 'echarts/core';
+import { BarChart, CandlestickChart, LineChart } from 'echarts/charts';
+import {
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  DataZoomComponent,
+  DataZoomInsideComponent,
+  DataZoomSliderComponent,
+} from 'echarts/components';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -12,9 +28,33 @@ import { CardComponent } from './gauges/card/card.component';
 import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
 import { TestComponent } from './test/test.component';
 
+import { ChartComponent } from './gauges/chart/chart.component';
+
+echarts.use([
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  LineChart,
+  CanvasRenderer,
+  BarChart,
+  CandlestickChart,
+  DataZoomComponent,
+  DataZoomInsideComponent,
+  DataZoomSliderComponent,
+]);
+echarts.registerTheme('macarons', Marcaron);
+
 @NgModule({
-  declarations: [AppComponent, NavBarComponent, CardComponent, GaugesComponent, TestComponent],
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    CardComponent,
+    GaugesComponent,
+    TestComponent,
+    ChartComponent,
+  ],
   imports: [
+    NgxEchartsModule.forRoot({ echarts }),
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
